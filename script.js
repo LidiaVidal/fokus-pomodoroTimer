3eaef2790cc0f876386b5497f7f080ab055bb583
LidiaVidal <lidiavidal0725@gmail.com>
2026-07-14 10:50:11 -0300

    Adiciona funcionalidades para a musica

--- script.js
+++ script.js
@@ -2,11 +2,24 @@ const html = document.querySelector("html");
 const focoBtn = document.querySelector(".app__card-button--foco");
 const curtoBtn = document.querySelector(".app__card-button--curto");
 const longoBtn = document.querySelector(".app__card-button--longo");
-
+const botoes = document.querySelectorAll('.app__card-button')
 const timer = document.querySelector("#timer");
 const banner = document.querySelector(".app__image");
 const titulo = document.querySelector(".app__title");
 const pauseBtn = document.querySelector("#start-pause");
+
+//Propriedades para o audio
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+musica.loop = true
+
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
 
 let timerFoco = 1500;
 let timerCurto = 300;
@@ -14,29 +27,35 @@ let timerLongo = 900;
 
 focoBtn.addEventListener("click", () => {
   alterarContexto("foco");
+  focoBtn.classList.add('active')
 });
 
 curtoBtn.addEventListener("click", () => {
   alterarContexto("descanso-curto");
+  curtoBtn.classList.add('active')
 });
 
 longoBtn.addEventListener("click", () => {
   alterarContexto("descanso-longo");
+  longoBtn.classList.add('active')
+
 });
 
 function alterarContexto(contexto) {
   html.setAttribute("data-contexto", contexto);
   banner.setAttribute("src", `/imagens/${contexto}.png`);
+  botoes.forEach(function (contexto) {
+    contexto.classList.remove('active')
+  })
   switch (contexto) {
     case "foco":
         titulo.innerHTML = ` Otimize sua produtividade,<br>
                 <strong class="app__title-strong">mergulhe no que importa.</strong>`
-        focoBtn.setAttribute('class', 'active')        
+            
         break;
     case "descanso-curto": 
         titulo.innerHTML = ` Que tal dar uma respirada?<br>
-                <strong class="app__title-strong">Faça uma pausa curta!</strong>`
-        curtoBtn.setAttribute('class', 'active')        
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`     
         
         break;
     case "descanso-longo":
@@ -45,6 +64,6 @@ function alterarContexto(contexto) {
         break;     
     default: 
         break;    
-
   }
+
 }
